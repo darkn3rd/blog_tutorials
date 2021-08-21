a0a5a076c3fc2e03463b3db0ec725262336b69bc
joaquin <joaquin@dgraph.io>
2021-08-20 17:30:04 -0700

    initial commit

---
+++ kubernetes/aks/series_0_provisioning/terraform/1_calico/main.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {}
+variable "location" {}
+variable "dns_prefix" {}
+variable "cluster_name" {}
+variable "create_group" { default = false }
+
+module "rg" {
+  source       = "../modules/group"
+  name         = var.resource_group_name
+  location     = var.location
+  create_group = var.create_group
+}
+
+module "aks" {
+  source              = "../modules/aks"
+  name                = var.cluster_name
+  dns_prefix          = var.dns_prefix
+  resource_group_name = var.resource_group_name
+
+  network_plugin      = "azure"
+  network_policy      = "calico"
+}
+
+output "resource_group_name" {
+  value = module.aks.resource_group_name
+}
+
+output "kubernetes_cluster_name" {
+  value = module.aks.kubernetes_cluster_name
+}
