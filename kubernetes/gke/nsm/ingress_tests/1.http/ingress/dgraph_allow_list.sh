44cf2761522b5f9b7492398aadc93a88c4550ab3
joaquin <joaquin@dgraph.io>
2022-08-28 02:04:54 -0700

    dgraph allow list for gke + ifconfig.me

--- kubernetes/gke/nsm/ingress_tests/1.http/ingress/dgraph_allow_list.sh
+++ kubernetes/gke/nsm/ingress_tests/1.http/ingress/dgraph_allow_list.sh
@@ -1,8 +1,9 @@
-DG_ALLOW_LIST=$(az aks show \
-  --name $AZ_CLUSTER_NAME \
-  --resource-group $AZ_RESOURCE_GROUP | \
-  jq -r '.networkProfile.podCidr,.networkProfile.serviceCidr' | \
-  tr '\n' ','
+DG_ALLOW_LIST=$(gcloud container clusters  describe $GKE_CLUSTER_NAME \
+  --project $GKE_PROJECT_ID \
+  --region $GKE_REGION \
+  --format json \
+  | jq -r '.clusterIpv4Cidr,.servicesIpv4Cidr' \
+  | tr '\n' ','
 )
 MY_IP_ADDRESS=$(curl --silent ifconfig.me)
 export DG_ALLOW_LIST="${DG_ALLOW_LIST}${MY_IP_ADDRESS}/32"
