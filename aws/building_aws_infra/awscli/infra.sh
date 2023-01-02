ea579f63996ca1d8bd75d2a9cbdf4cb72a98ade3
Joaquin Menchaca <joaquin@dgraph.io>
2023-01-02 23:40:51 +0100

    WIP: buildling-aws-infra/awscli: igwd, route tables

--- aws/building_aws_infra/awscli/infra.sh
+++ aws/building_aws_infra/awscli/infra.sh
@@ -35,7 +35,7 @@ aws ec2 modify-subnet-attribute \
 JSON_DATA=$(aws ec2 create-subnet \
   --vpc-id $VPC_ID \
   --availability-zone ${AZ_ZONES[1]} \
-  --cidr-block "10.0.2.0/24"
+  --cidr-block "10.0.2.0/24" \
   --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$USER-public2},{Key=Sitetring,Value=$USER-web-site}]"
 )
 
@@ -44,22 +44,19 @@ aws ec2 modify-subnet-attribute \
   --map-public-ip-on-launch \
   --subnet-id $SUBNET_ID
 
-# get subnet id
-
-
 ################
 # Private Subnets
 ############################
 aws ec2 create-subnet \
   --vpc-id $VPC_ID \
   --availability-zone ${AZ_ZONES[0]} \
-  --cidr-block "10.0.3.0/24"
+  --cidr-block "10.0.3.0/24" \
   --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$USER-private1},{Key=Sitetring,Value=$USER-web-site}]"
 
 aws ec2 create-subnet \
   --vpc-id $VPC_ID \
   --availability-zone ${AZ_ZONES[1]} \
-  --cidr-block "10.0.4.0/24"
+  --cidr-block "10.0.4.0/24" \
   --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$USER-private2},{Key=Sitetring,Value=$USER-web-site}]"
 
 
@@ -70,26 +67,46 @@ JSON_DATA=$(aws ec2 create-internet-gateway \
 ################
 # Internet Gateway 
 ############################
-aws ec2 attach-internet-gateway \
-  --internet-gateway-id <value>
-  --vpc-id $VPC_ID
+JSON_DATA=$(aws ec2 create-internet-gateway \
+  --tag-specifications \
+   "ResourceType=internet-gateway,Tags=[{Key=Name,Value=$USER-igw},{Key=Sitetring,Value=$USER-web-site}]"
+)
 
+IGW_ID=$(jq -r .InternetGateway.InternetGatewayId <<< $JSON_DATA)
+
+aws ec2 attach-internet-gateway \
+  --internet-gateway-id $IGW_ID \
+  --vpc-id $VPC_ID
 
 ################
 # Route Table
 ############################
-aws ec2 create-route-table \
-  --vpc-id $VPC_ID
+JSON_DATA=$(aws ec2 create-route-table --vpc-id $VPC_ID)
+RT_ID=$(jq -r .RouteTable.RouteTableId <<< $JSON_DATA)
 
 aws ec2 create-route \
-  --route-table-id <value>
+  --route-table-id $RT_ID \
   --destination-cidr-block "0.0.0.0/0" \
-  --gateway-id <value>
+  --gateway-id $IGW_ID
+
+SUBNET_ID=$(aws ec2 describe-subnets \
+  --filter "Name=vpc-id,Values=$VPC_ID" \
+  --filter "Name=tag:Name,Values=$USER-public1" \
+  --query 'Subnets[0].SubnetId' \
+  --output text
+)
 
 aws ec2 associate-route-table \
-  --route-table-id <value> \
-  --subnet-id <value>
+  --route-table-id $RT_ID \
+  --subnet-id $SUBNET_ID
+
+SUBNET_ID=$(aws ec2 describe-subnets \
+  --filter "Name=vpc-id,Values=$VPC_ID" \
+  --filter "Name=tag:Name,Values=$USER-public2" \
+  --query 'Subnets[0].SubnetId' \
+  --output text
+)
 
 aws ec2 associate-route-table \
-  --route-table-id <value> \
-  --subnet-id <value>
+  --route-table-id $RT_ID \
+  --subnet-id $SUBNET_ID
