e3d0c282bc06e6d136e30b64350d1177896cd073
Joaquin Menchaca <joaquin@dgraph.io>
2026-06-26 22:17:31 -0700

    gwtcp

---
+++ kubernetes/eks/baseline/addons/aws_load_balancer_controller/demos/03.gw_nlb/gwtcp.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+kubectl create deployment demo-gwtcp-app \
+  --image=nginx:alpine
+
+kubectl expose deployment demo-gwtcp-app \
+  --port=80 \
+  --target-port=80
+
+cat <<EOF | kubectl apply -f -
+apiVersion: gateway.networking.k8s.io/v1
+kind: GatewayClass
+metadata:
+  name: aws-nlb-class
+spec:
+  controllerName: gateway.k8s.aws/nlb
+---
+apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: demo-nlb-gateway
+  namespace: demo-gwtcp
+spec:
+  gatewayClassName: aws-nlb-class
+  infrastructure:
+    parametersRef:
+      group: gateway.k8s.aws
+      kind: LoadBalancerConfiguration
+      name: demo-nlb-lg-config
+  listeners:
+    - name: tcp-80
+      protocol: TCP
+      port: 80
+      allowedRoutes:
+        namespaces:
+          from: Same
+        kinds:
+          - kind: TCPRoute  
+---
+apiVersion: gateway.networking.k8s.io/v1alpha2
+kind: TCPRoute
+metadata:
+  name: demo-nlb-route
+  namespace: demo-gwtcp
+spec:
+  parentRefs:
+    - name: demo-nlb-gateway
+      sectionName: tcp-80
+  rules:
+    - backendRefs:
+        - name: demo-gwtcp-app
+          kind: Service
+          port: 80
+---
+apiVersion: gateway.k8s.aws/v1beta1
+kind: LoadBalancerConfiguration
+metadata:
+  name: demo-nlb-lg-config
+  namespace: demo-gwtcp
+spec:
+  scheme: internet-facing
+---
+apiVersion: gateway.k8s.aws/v1beta1
+kind: TargetGroupConfiguration
+metadata:
+  name: demo-nlb-tg-config
+  namespace: demo-gwtcp
+spec:
+  targetReference:
+    group: ""
+    kind: Service
+    name: demo-gwtcp-app
+  defaultConfiguration:
+    targetType: ip
+EOF
